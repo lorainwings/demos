28ce1a0a1ab9b1254da7f7f1d677811b4f8e1d52
lorain <lorainwings@gmail.com>
2024-06-04 01:05:52 +0800

    feat: 🎸 update

--- database/mongodb-article/app.ts
+++ database/mongodb-article/app.ts
@@ -1,13 +1,12 @@
 import express from "express"
+import errorHandler  from './middleware'
+import articlesRouter from './router/articles.router'
 
 const app = express()
+app.use(express.json())
+app.use('/articles', articlesRouter)
+app.use(errorHandler)
 
-
-console.log(123)
-
-app.get('/', (req, res) => {
-  res.send('Hello World！')
-})
 
 app.listen(3000, () => {
   console.log('app listenning at port 3000.')
---
+++ database/mongodb-article/config/index.ts
@@ -0,0 +1,3 @@
+const uri = 'mongodb://localhost:27017'
+
+export { uri }
